bc2078b8b08004ba6f6ae4d04f1f2ab0c754d1a0
unknown <arathikvijay@gmail.com>
2024-09-02 09:33:48 -0700

    Launch

--- lib/rocket.dart
+++ lib/rocket.dart
@@ -1,28 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
-
-class rocket extends StatefulWidget {
-  const rocket({super.key});
+class Rocket extends StatefulWidget {
+  const Rocket({super.key});
 
   @override
- // State<rocket> createState() => _rocketState();
-  _rocketState createState()=>_rocketState();
-   //_LottiePageState createState() => _LottiePageState(); 
+  RocketState createState() => RocketState();
 }
 
-class _rocketState extends State<rocket> {
+class RocketState extends State<Rocket> {
+  bool _showRocket = false; // Boolean to control animation visibility
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Center(
+      appBar: AppBar(
+        title: const Text('Rocket Animation'),
+      ),
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Lottie.network("https://lottie.host/9c48b2b7-a9f1-4e23-84f8-bf0d3e2c257c/xui4OemRxk.json")
+            // Conditionally display the Lottie animation
+            if (_showRocket)
+              Lottie.network(
+                "https://lottie.host/9c48b2b7-a9f1-4e23-84f8-bf0d3e2c257c/xui4OemRxk.json",
+                width: 300, // Optionally adjust width
+                height: 300, // Optionally adjust height
+              ),
+            const SizedBox(height: 20), // Space between the animation and button
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _showRocket = !_showRocket; // Toggle animation visibility
+                });
+              },
+              child: Text(_showRocket ? 'Hide' : 'Launch'),
+            ),
           ],
         ),
-      ) ,
+      ),
     );
   }
 }
